a92dad33a0cae4a1fb6f6a325d5219f2f136fa06
Valentina Vorfolomeeva <vorfolomeevav@gmail.com>
2021-01-19 15:41:33 +0200

    Updates

---
+++ precache-manifest.191bdac4c80fbb8506bd56e1ab05b0e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788c9f2c90db1505156376dc3e0d2ee3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cd779ebea3678ddbf3b2",
+    "url": "./static/css/2.9634ee28.chunk.css"
+  },
+  {
+    "revision": "48ed1f49c04a2c1f6be7",
+    "url": "./static/css/main.bada76a3.chunk.css"
+  },
+  {
+    "revision": "cd779ebea3678ddbf3b2",
+    "url": "./static/js/2.6f705819.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6f705819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ed1f49c04a2c1f6be7",
+    "url": "./static/js/main.1ecb566d.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
